72c7488b33deb51d0fd72773eba1d4436df66e9f
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-07 16:39:50 +0530

    [feat]: Add Hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 const Page = () => {
   return (
     <div>
-      <Navbar />
+      <div className="relative pt-6">
+        <Navbar />
+        <Hero />
+        <Image
+          src="/mainbg.png"
+          width={704}
+          height={724}
+          alt="mainbg"
+          className="absolute top-0 right-0 -z-20 w-[35rem]"
+        />
+      </div>
     </div>
   );
 };
