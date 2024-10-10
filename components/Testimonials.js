01c02e9e47d9d6246448c1f63ea356b469920322
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-10 15:49:05 +0530

    add another testimonial

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Testimonial = () => {
   return (
     <div className="flex max-w-6xl mx-auto my-40 justify-around">
-      <div className="flex flex-col gap-10 max-w-[20rem]">
+      <div className="flex z-10 flex-col gap-10 max-w-[20rem]">
         <div className="space-y-2">
           <h2 className="text-gray">TESTIMONIAL</h2>
           <h1 className="text-4xl font-volkhov font-bold">
@@ -17,7 +17,7 @@ const Testimonial = () => {
       </div>
 
       <div className="flex items-center relative">
-        <div className="shadow-xl p-4 rounded-3xl flex">
+        <div className="shadow-xl bg-white p-4 rounded-3xl flex">
           <div>
             <Image
               src="/testimonial.png"
@@ -39,9 +39,33 @@ const Testimonial = () => {
             </div>
           </div>
         </div>
+
         <div className="space-y-6 absolute bottom-6 -right-20 ">
-          <ChevronUp className="text-gray" size={18}/>
-          <ChevronDown size={18}/>
+          <ChevronUp className="text-gray" size={18} />
+          <ChevronDown size={18} />
+        </div>
+
+        <div className="absolute -bottom-16 left-14 -z-10 shadow-xl p-4 rounded-3xl flex bg-[#F7F7F7]">
+          <div>
+            <Image
+              src="/testimonial.png"
+              width={76}
+              height={76}
+              alt="testimonial"
+              className=""
+            />
+          </div>
+          <div className="space-y-6 pt-16">
+            <p className="text-gray text-sm max-w-sm">
+              “On the Windows talking painted pasture yet its express parties
+              use. Sure last upon he same as knew next. Of believed or diverted
+              no.”
+            </p>
+            <div className="space-y-1">
+              <h1 className="text-gray">Chris Thomas</h1>
+              <p className="text-gray text-sm">CEO of Red Button</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
